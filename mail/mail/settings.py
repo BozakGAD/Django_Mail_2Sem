1264545107e8ffd29570b23bd8666e9198d9dcce
Bozak <alexplayppap.a@gmail.com>
2026-05-06 19:39:52 +0500

    fix ui bugs

--- mail/mail/settings.py
+++ mail/mail/settings.py
@@ -68,5 +68,8 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
